e6c78b172436729527e6a3be5383e31893e2ac2a
enkeljon gjeta <enkelionimarku@gmail.com>
2025-02-08 22:22:01 +0100

    lets see this

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const home = new rive.Rive({
-  src: "resources/home_button_text.riv",
+  src: "/Enkeljon-Gjeta/resources/home_button_text.riv",
   canvas: document.getElementById("home"),
   autoplay: true,
   stateMachines: "home_state",
@@ -9,7 +9,7 @@ const home = new rive.Rive({
 });
 
 const story = new rive.Rive({
-  src: "resources/story_button_text.riv",
+  src: "/Enkeljon-Gjeta/resources/story_button_text.riv",
   canvas: document.getElementById("story"),
   autoplay: true,
   stateMachines: "home_state",
