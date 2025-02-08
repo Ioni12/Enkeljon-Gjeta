0dc53ee925b6aafeedf8585dbda38f909cc05e32
enkeljon gjeta <enkelionimarku@gmail.com>
2025-02-08 23:48:44 +0100

    i made something with the buttons

--- main.js
+++ main.js
@@ -1,5 +1,15 @@
+const getBasePath = () => {
+  if (window.location.hostname.includes("github.io")) {
+    return "/Enkeljon-Gjeta";
+  }
+
+  return "";
+};
+
+const baseUrl = getBasePath();
+
 const home = new rive.Rive({
-  src: "/Enkeljon-Gjeta/resources/home_button_text.riv",
+  src: `${baseUrl}/resources/home_button_text.riv`,
   canvas: document.getElementById("home"),
   autoplay: true,
   stateMachines: "home_state",
@@ -9,11 +19,28 @@ const home = new rive.Rive({
 });
 
 const story = new rive.Rive({
-  src: "/Enkeljon-Gjeta/resources/story_button_text.riv",
+  src: `${baseUrl}/resources/story_button_text.riv`,
   canvas: document.getElementById("story"),
   autoplay: true,
   stateMachines: "home_state",
   onLoad: () => {
     story.resizeDrawingSurfaceToCanvas();
   },
+});
+
+const home_div = document.getElementById("home_div");
+const story_div = document.getElementById("story_div");
+
+home_div.addEventListener("click", () => {
+  home_div.classList.add("background");
+  home_div.classList.remove("noBackground");
+  story_div.classList.remove("background");
+  story_div.classList.add("noBackground");
+});
+
+story_div.addEventListener("click", () => {
+  story_div.classList.add("background");
+  story_div.classList.remove("noBackground");
+  home_div.classList.remove("background");
+  home_div.classList.add("noBackground");
 });
